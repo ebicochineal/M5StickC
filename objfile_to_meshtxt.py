1664f837bbc0d0a3c440e440ebdd06e05cd7e187
ebicochineal <yz94588@gmail.com>
2019-11-04 22:27:16 +0900

    add

---
+++ objfile_to_meshtxt.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python3
+import os
+
+path = input('obj_file_path = ')
+
+vertex = []
+face = []
+with open(path) as f:
+    toint = lambda x : int(x.split('/')[0])
+    for i in f.readlines():
+        if str(i).startswith('v '):
+            _, x, y, z = i.split(' ')
+            vertex += [[float(x), float(y), float(z)]]
+        if i.startswith('f '):
+            *l, = i.split(' ')
+            if len(l) == 4:
+                _, a, b, c = l
+                face += [[toint(a)-1, toint(b)-1, toint(b)-1]]
+            else:
+                _, a, b, c, d = l
+                face += [[toint(a)-1, toint(b)-1, toint(d)-1]]
+                face += [[toint(b)-1, toint(c)-1, toint(d)-1]]
+
+# (this script dirpath)/mesh.txt
+p = os.path.dirname(__file__).replace('\\', '/') + '/'
+p = '' if p == '/' else p
+with open(p+'mesh.txt', 'w') as f:
+    for i in vertex:
+        f.write('mesh.addVertex(' + ', '.join(str(x) for x in i) + ');\n')
+    for i in face:
+        f.write('mesh.addFace(' + ', '.join(str(x) for x in i) + ');\n')
+
+
+
+
+
+
+
+
+
